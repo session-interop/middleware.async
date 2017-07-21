21c3dfd902c59e7a66cf0e815fc02e9f00ea0b7d
Benoît Ngô <ngobenoit@gmail.com>
2017-07-21 19:40:04 +0200

    Adding registering middleware in the middleware pipe allowing this package to work with thecodingmachine/middleware-list-universal-module

--- src/AsyncSessionMiddlewareServiceProvider.php
+++ src/AsyncSessionMiddlewareServiceProvider.php
@@ -6,6 +6,9 @@ use Interop\Container\ContainerInterface as Container;
 use Interop\Session\Manager\SessionManagerInterface;
 use Interop\Container\ServiceProvider;
 
+use TheCodingMachine\MiddlewareListServiceProvider;
+use TheCodingMachine\MiddlewareOrder;
+
 class AsyncSessionMiddlewareServiceProvider implements ServiceProvider
 {
     public function getServices()
@@ -17,6 +20,19 @@ class AsyncSessionMiddlewareServiceProvider implements ServiceProvider
             AsyncSessionMiddleware::class => function (Container $container) {
                 return new AsyncSessionMiddleware($container->get(AsyncSessionFactory::class));
             },
+            MiddlewareListServiceProvider::MIDDLEWARES_QUEUE => [self::class, 'updatePriorityQueue'],
         ];
+    } 
+    public static function updatePriorityQueue(Container $container, callable $previous = null) : \SplPriorityQueue
+    {
+        if ($previous) {
+            $priorityQueue = $previous();
+            $priorityQueue->insert($container->get(AsyncSessionMiddleware::class), MiddlewareOrder::UTILITY_EARLY);
+            return $priorityQueue;
+        } else {
+            throw new \InvalidArgumentException("Could not find declaration for service '".MiddlewareListServiceProvider::MIDDLEWARES_QUEUE."'.");
+        }
     }
+
 }
+ 
